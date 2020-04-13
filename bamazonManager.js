3e87c47c4da755ee06b058df7487b5ebb80c92e2
Hung Le <hungle913@gmail.com>
2020-04-13 10:51:27 -0700

    Created new manager.js file. Added starter code copied from customer.js

---
+++ bamazonManager.js
@@ -0,0 +1,21 @@
+//require mysql and inquirer
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+// create connection information to sql database
+var connection = mysql.createConnection({
+    host: "localhost",
+
+    // Port number for connection
+    port:3306,
+
+    // Username for connection
+    user: "root",
+
+    // Password to connect
+    password: "password",
+
+    //database to connect to
+    database: "bamazon_DB"
+});
+
